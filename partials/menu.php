a00bf0ce2ffe602ca7f4bbd3de7de8325825833a
Anugrah Sandy Sulman Pratama <anugrahsandysulmanpratama@gmail.com>
2025-10-11 23:24:29 +0700

    tukar file dengan yang sudah di compile

---
+++ partials/menu.php
@@ -0,0 +1,4 @@
+<!-- Topbar Start -->
+<?php
+include __DIR__ . '/topbar.php';
+include __DIR__ . '/sidebar.php';
---
+++ partials/page-title.php
@@ -0,0 +1,16 @@
+<!-- ========== Page Title Start ========== -->
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            <h4 class="mb-0"><?= htmlspecialchars($title ?? 'PAMSIMAS', ENT_QUOTES, 'UTF-8') ?></h4>
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a
+                        href="javascript: void(0);"><?= htmlspecialchars($title ?? 'PAMSIMAS', ENT_QUOTES, 'UTF-8') ?></a>
+                </li>
+                <li class="breadcrumb-item active">
+                    <?= htmlspecialchars($subTitle ?? 'Default Sub Title', ENT_QUOTES, 'UTF-8') ?></li>
+            </ol>
+        </div>
+    </div>
+</div>
+<!-- ========== Page Title End ========== -->
